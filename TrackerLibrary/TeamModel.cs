2d390561bc3ad5efb13a75227acb7f6f81d58b94
Taskoudis Dimitris <taskoudisdimitris@gmail.com>
2022-03-21 23:42:19 +0200

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        //Έχω δύο properties
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+    
+
+    } 
+}
