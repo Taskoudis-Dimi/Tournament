e031e895b840e898216456c885b42952db4a8c5c
Taskoudis Dimitris <taskoudisdimitris@gmail.com>
2022-04-17 21:52:18 +0300

    home

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
 namespace TrackerUI
 {
     internal static class Program
@@ -10,7 +19,9 @@ namespace TrackerUI
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            //ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
 
             TrackerLibrary.GlobalConfig.InitializeConnections(true, true);
